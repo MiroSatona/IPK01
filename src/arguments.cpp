872b5da8baab319cc2684410a3b27f2f5a39c7af
Martin Zůbek <martinzubek@email.cz>
2025-02-28 20:55:31 +0100

    feat: change structure of arguments , this will be mby done

--- src/arguments.cpp
+++ src/arguments.cpp
@@ -5,27 +5,33 @@
 #include <netdb.h>
 #include <cstring>
 #include <net/if.h>
+#include <unordered_set>
 
 Arguments::Arguments(std::string interface, std::string domain, std::string tcpPorts, std::string udpPorts, std::string timeout, bool parsedTcp, bool parsedUdp, TYPE_SCAN typeScan)
-    : interface(interface), domain(domain){
-    if(typeScan == INTERACES_ONLY){
-        this->valid = true;
-        return;
-    }
+    : interface(interface), domainName(domain){
+    if(typeScan == INTERACES_ONLY)return;
+
+
     this->timeout = DEFAULT_TIMEOUT;
-    this->valid = false;
     this->tcpPorts = this->convertPorts(tcpPorts);
     this->udpPorts = this->convertPorts(udpPorts);
-    this->valid = true;
-    this ->valid = this->validateInterface() && this->validateDomain() && this->validatePorts(parsedTcp, parsedUdp) && this->validateTimeout(timeout); 
+    this->validateInterface();
+    this->setTypeDomain(); 
+    this->validateDomain(); 
+    this->validatePorts(parsedTcp, parsedUdp); 
+    this->validateTimeout(timeout); 
 }
 
 std::string Arguments::getInterface(){ 
     return this->interface; 
 }
 
-std::string Arguments::getDomain(){ 
-    return this->domain; 
+std::unordered_set<std::string> Arguments::getIp4Addr(){ 
+    return this->ip4Addr; 
+}
+
+std::unordered_set<std::string> Arguments::getIp6Addr(){ 
+    return this->ip6Addr; 
 }
 
 std::vector<int> Arguments::getPotsTcp(){ 
@@ -39,11 +45,6 @@ std::vector<int> Arguments::getPotsUdp(){
 int Arguments::getTimeout(){ 
     return this->timeout; 
 }
-
-bool Arguments::isValid(){ 
-    return this->valid; 
-}
-
 std::string Arguments::getDomainName(){ 
     return this->domainName; 
 }
@@ -54,15 +55,17 @@ std::vector<int> Arguments::convertPorts(std::string convertPorts){
     std::regex portsRange("^(?:(?:6553[0-5]|655[0-2]\\d|65[0-4]\\d{2}|6[0-4]\\d{3}|[1-5]?\\d{1,4}))-(?:(?:6553[0-5]|655[0-2]\\d|65[0-4]\\d{2}|6[0-4]\\d{3}|[1-5]?\\d{1,4}))$");
 
     std::vector<int> result = {};
-
+    bool possDuplicate = false;
     if (std::regex_match(convertPorts, port)){
         result.push_back(std::stoi(convertPorts));
     }
     else if (std::regex_match(convertPorts, ports)) {
         std::istringstream charactersStream(convertPorts);
         std::string character;
+        
         while (std::getline(charactersStream, character, ',')) {
             result.push_back(std::stoi(character));
+            possDuplicate = true;
         }
     } 
     else if (std::regex_match(convertPorts, portsRange)) {
@@ -73,22 +76,32 @@ std::vector<int> Arguments::convertPorts(std::string convertPorts){
             result.push_back(intervalBegin);
             intervalBegin++;
         }
-    } 
+    }
+
+
+    for (size_t i = 0; i < result.size() && possDuplicate; i++) {
+        for (size_t j = i + 1; j < result.size(); j++) {
+            if (result[i] == result[j]) throw std::invalid_argument("Port was pasted more than once!");
+        }
+    }
+
 
     return result;
 }
 
-bool Arguments::validateInterface(){
+void Arguments::validateInterface(){
 
-    if (this->interface.empty()) return false;
+    if (this->interface.empty()){
+        throw std::invalid_argument("Interface was not pasted!");
+        return;
+    }
         
     
     struct ifaddrs *listIntefaces;
-    
     if (getifaddrs(&listIntefaces) == -1) {
-        
-        return false;
+        return;
     }
+
     bool found = false;
     for (struct ifaddrs *interface = listIntefaces; interface != nullptr; interface = interface->ifa_next) {
         if (interface->ifa_name && this->interface == interface->ifa_name && (interface->ifa_flags & IFF_UP)){
@@ -98,102 +111,123 @@ bool Arguments::validateInterface(){
     }
 
     freeifaddrs(listIntefaces);
-    return found;
+    if(!found) throw std::invalid_argument("Interface is invalid or is not active!");
+    return;
 }
 
-bool Arguments::validateDomain(){
-    setTypeDomain();
-    if(this->domainType == INVALID) return false;
-
+void Arguments::validateDomain(){
+   
+    struct addrinfo hints, *listOfAddrInfo;
+    memset(&hints, 0, sizeof(hints));
+    
     switch (this->domainType){
-        case DOMAIN:
-            struct addrinfo hints, *domainInfoResult;
-
-            memset(&hints, 0, sizeof(hints));
-
-            hints.ai_family = AF_UNSPEC;
-
-            if(!getaddrinfo(this->domainName.c_str(), nullptr, &hints, &domainInfoResult)) {
-
-                void* addr = nullptr;
-            
-                for (struct addrinfo* element = domainInfoResult; element != nullptr; element = element->ai_next) {
-            
-                    if (element->ai_family == AF_INET) {
-                        this->domainType = IPV4;
-            
-                        sockaddr_in* ipv4 = reinterpret_cast<sockaddr_in*>(element->ai_addr);
-                        addr = &(ipv4->sin_addr);
-                        char ipv[INET_ADDRSTRLEN];
-                        inet_ntop(element->ai_family, addr, ipv, INET_ADDRSTRLEN);
-                        this->domain = std::string(ipv);
-                        break;
-    
-                    } else if(element->ai_family == AF_INET6){
-                        this->domainType = IPV6;
-                        sockaddr_in6* ipv6 = reinterpret_cast<sockaddr_in6*>(element->ai_addr);
-                        addr = &(ipv6->sin6_addr);
-                        char ipv[INET6_ADDRSTRLEN];
-                        inet_ntop(element->ai_family, addr, ipv, INET6_ADDRSTRLEN);
-                        this->domain = std::string(ipv);
-                    }
-    
-                }
-
-                freeaddrinfo(domainInfoResult);
-                return true;
-    
-            } else {
-    
-                return false;
-    
-            }
-    
-            break;
-    default:
-
-
-        return true;
+    case INVALID:
+        throw::std::invalid_argument("Invalid domain was pasted!");
+        break;
+    case IPV4:
+        hints.ai_family = AF_INET;
+        break;
+    case IPV6:
+        hints.ai_family = AF_INET6;
+        break;   
+    case DOMAIN:
+        hints.ai_family = AF_UNSPEC;
         break;
     }
+
+    int retVal = getaddrinfo(this->domainName.c_str(), nullptr, &hints, &listOfAddrInfo);
+    
+    if(retVal == EAI_NONAME) throw std::invalid_argument("Invalid domain name was pasted!");
+    else if(retVal || listOfAddrInfo == nullptr) throw std::invalid_argument("Internal error of getaddrinfo!");
+
+
+    switch (this->domainType){
+        case DOMAIN:{ 
+
+        void* addr = nullptr;
+        for (struct addrinfo* element = listOfAddrInfo; element != nullptr; element = element->ai_next) {
+    
+            if (element->ai_family == AF_INET) {
+                sockaddr_in* ipv4 = reinterpret_cast<sockaddr_in*>(element->ai_addr);
+                addr = &(ipv4->sin_addr);
+                char ipv[INET_ADDRSTRLEN];
+                inet_ntop(element->ai_family, addr, ipv, INET_ADDRSTRLEN);
+                this->ip4Addr.insert(std::string(ipv));
+
+            } else if(element->ai_family == AF_INET6){
+                sockaddr_in6* ipv6 = reinterpret_cast<sockaddr_in6*>(element->ai_addr);
+                addr = &(ipv6->sin6_addr);
+                char ipv[INET6_ADDRSTRLEN];
+                inet_ntop(element->ai_family, addr, ipv, INET6_ADDRSTRLEN);
+                this->ip6Addr.insert(std::string(ipv));
+            }
+
+        }
+        break;
+    }
+    default:{ 
+        char hostname[NI_MAXHOST];
+        int retValName = getnameinfo(listOfAddrInfo->ai_addr, listOfAddrInfo->ai_addrlen, hostname, NI_MAXHOST, nullptr, 0, NI_NAMEREQD);
+        
+        if(retValName == EAI_NONAME) {
+            freeaddrinfo(listOfAddrInfo);
+            throw std::invalid_argument("Invalid domain address was pasted!");
+        }
+        else if(retValName){
+            freeaddrinfo(listOfAddrInfo);
+            throw std::invalid_argument("Internal error of getnameinfo!");
+        }
+        this->domainName = std::string(hostname);
+        break;
+        }
+    }
+    freeaddrinfo(listOfAddrInfo);
+    return;
 }
+
 
 
 void Arguments::setTypeDomain(){
 
-    if (this->domain.empty()) this->domainType = INVALID;
+    if (this->domainName.empty()) this->domainType = INVALID;
     std::regex ipv4Reg("^((25[0-5]|2[0-4]\\d|[01]?\\d?\\d)\\.){3}(25[0-5]|2[0-4]\\d|[01]?\\d?\\d)$");
     std::regex ipv6Reg("^((?:[0-9A-Fa-f]{1,4}:){7}[0-9A-Fa-f]{1,4}|(?:[0-9A-Fa-f]{1,4}:){1,7}:|(?:[0-9A-Fa-f]{1,4}:){1,6}:[0-9A-Fa-f]{1,4}|(?:[0-9A-Fa-f]{1,4}:){1,5}(?::[0-9A-Fa-f]{1,4}){1,2}|(?:[0-9A-Fa-f]{1,4}:){1,4}(?::[0-9A-Fa-f]{1,4}){1,3}|(?:[0-9A-Fa-f]{1,4}:){1,3}(?::[0-9A-Fa-f]{1,4}){1,4}|(?:[0-9A-Fa-f]{1,4}:){1,2}(?::[0-9A-Fa-f]{1,4}){1,5}|[0-9A-Fa-f]{1,4}:(?::[0-9A-Fa-f]{1,4}){1,6}|:(?::[0-9A-Fa-f]{1,4}){1,7}|:)$");
     this->domainType = INVALID;
 
-    if(std::regex_match(domain, ipv4Reg)){
+    if(std::regex_match(domainName, ipv4Reg)){
         this->domainType = IPV4;
-        this->domainName = "";
+        this->ip4Addr.insert(domainName);
+        
     }
-    else if(std::regex_match(domain, ipv6Reg)){
+    else if(std::regex_match(domainName, ipv6Reg)){
         this->domainType = IPV6;
-        this->domainName = "";
-    
+        this->ip6Addr.insert(domainName);
+        
     }
     else {
-       
         this->domainType = DOMAIN;
-        this->domainName = domain;
-        this->domain = "";
-    
     }
 }
 
 
 
-bool Arguments::validatePorts(bool tcp, bool udp){
-    return (!this->tcpPorts.empty() == tcp) && (!this->udpPorts.empty() == udp) && (!this->tcpPorts.empty() || !this->udpPorts.empty());
+void Arguments::validatePorts(bool tcp, bool udp){
+
+    bool tcpEmpty = this->tcpPorts.empty();
+    bool udpEmpty = this->udpPorts.empty();
+    bool invalidPortsSyntax = (tcpEmpty && tcp) || (udpEmpty && udp);
+    bool notPorts = !tcp && !udp;
+    
+    if (invalidPortsSyntax) throw std::invalid_argument("Invalid input of ports!");
+    else if (notPorts) throw std::invalid_argument("Invalid input of ports!");
+    else return;
 }
 
-bool Arguments::validateTimeout(std::string time){
-    if (time.empty()) return true;
+void Arguments::validateTimeout(std::string time){
+    if (time.empty()) return;
     std::regex timeReg("^[1-9][0-9]*$");
     if(std::regex_match(time, timeReg)) this->timeout = std::stoi(time);
-    else this->timeout = 0;
-    return this->timeout > 0;
+    else throw std::invalid_argument("Invalid timeout was pasted!");
+
+    return;
 }
--- src/arguments.hpp
+++ src/arguments.hpp
@@ -4,27 +4,39 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_set>
 
-
+// Default timeout
 #define DEFAULT_TIMEOUT 5000
 
+
+// Enum for set if scanner will print interfaces only or scan
 enum TYPE_SCAN{
     INTERACES_ONLY,
     DEFUALT_SCAN
 };
 
+// Type of pasted domain
 enum IPV_TYPE{
     IPV4,
     IPV6,
     DOMAIN,
-    INVALID
+    INVALID,
 };
 
 
-
+/**
+ * @class Arguments
+ * @brief Class for save and validate arguments.
+ * 
+ * 
+ * Public methods of class are use for get arguments after validation.
+ * Private methods and variables of class for validate arguments and save them.
+ */
 class Arguments{
     public:
         
+
         Arguments(std::string interface, std::string domain, std::string tcpPorts, std::string udpPorts, std::string timeout, bool parsedTcp, bool parsedUdp, TYPE_SCAN typeScan);  
 
         std::string getDomainName();
@@ -34,26 +46,27 @@ class Arguments{
         std::vector<int> getPotsUdp(); 
         IPV_TYPE getDomainType();
         int getTimeout();
-        bool isValid();
-       
+        std::unordered_set<std::string> getIp4Addr();
+        std::unordered_set<std::string> getIp6Addr();
 
     private:
 
-        bool valid;
+       
         std::string interface;
         IPV_TYPE domainType;
-        std::string domain;
+        std::unordered_set<std::string> ip4Addr;
+        std::unordered_set<std::string> ip6Addr;
         std::string domainName;
         int timeout;
         std::vector<int> tcpPorts;
         std::vector<int>  udpPorts;
       
-        
+        // Private methods for validate arguments or convert them
         std::vector<int> convertPorts(std::string ports);   
-        bool validateInterface();
-        bool validateDomain();
-        bool validatePorts(bool tcp, bool udp);
-        bool validateTimeout(std::string time);
+        void validateInterface();
+        void validateDomain();
+        void  validatePorts(bool tcp, bool udp);
+        void validateTimeout(std::string time);
         void setTypeDomain();
         
 
