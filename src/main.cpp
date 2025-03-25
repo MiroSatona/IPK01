3e892c82121465b2fcc97dce26df83defd80d4e5
Martin Zůbek <martinzubek@email.cz>
2025-03-25 02:39:05 +0100

    feat: added err handling and documentaion of code

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
+/**
+ * @file main.cpp
+ * @name: Martin Zůbek, x253206
+ * @date 25.3. 2025
+ * @brief Main file for the IPK project 1 [OMEGA] 
+ */
+
 #include <iostream>
 #include "parser_arguments.hpp"
 #include "command.hpp"
 #include "scanner_params.hpp"
 #include "scanner.hpp"
+#include "return_values.hpp"
 
+int main(int argc, char *argv[]){
 
-int main(int argc, char *argv[])
-{
    try{ 
       // Parse arguments
       ParseArguments args(argc, argv);
@@ -33,24 +40,33 @@ int main(int argc, char *argv[])
          TcpIpv4Scanner tcpIpv4(scanParams);
          tcpIpv4.scan();
       }
+      
       if (!scanParams.getTcpPorts().empty() && !scanParams.getIp6AddrDest().empty()){
          TcpIpv6Scanner tcpIpv6(scanParams);
          tcpIpv6.scan();
       }
+      
       if (!scanParams.getUdpPorts().empty() && !scanParams.getIp4AddrDest().empty()){
          UdpIpv4Scanner udpIpv4(scanParams);
          udpIpv4.scan();
       }
+
       if (!scanParams.getUdpPorts().empty() && !scanParams.getIp6AddrDest().empty()){
          UdpIpv6Scanner udpIpv6(scanParams);
          udpIpv6.scan();
       }
+   
+   }
+   // Catch error of invlaid input
+   catch (const std::invalid_argument&) {
+   std::cerr << "Error: Invalid input was pasted!" << std::endl;
+   return INVALID_ARGUMENTS;
+   // Catch internal error of program
+   }catch (const std::runtime_error& e) {
+   std::cerr << "Error: " << e.what() << std::endl;
+   return INTERNAL_ERROR;
+   }
 
-   }
-   catch(std::exception &e){
-      std::cerr << "Error: " << e.what() << std::endl;
-      return 1;
-   }
-    
-   return 0;
+
+   return SUCCESS;
 }
